79c89f22e6a0a6ddcf51018667fa2012a0c25b01
saatvik-10 <saatvikmadan529@gmail.com>
2025-02-28 18:32:40 +0530

    added get route

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -35,10 +37,62 @@ func main() {
 		log.Fatal(err)
 	}
 
+	defer client.Disconnect(context.Background())
+
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database("todo-GoLang_db").Collection("todos")
+
+	app := fiber.New()
+
+	app.Get("/api/todos", getTodos)
+	// app.Post("/api/todos", createTodo)
+	// app.Patch("/api/todos/:id", updateTodo)
+	// app.Delete("/api/todos/:id", deleteTodo)
+
+	PORT := os.Getenv("PORT")
+
+	if PORT == "" {
+		PORT = "4000"
+	}
+
+	log.Fatal((app.Listen(":" + PORT)))
 }
+
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo
+
+	cursor, err := collection.Find(context.Background(), bson.M{}) //cursor to iterate through the results
+
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) { //Iterates through results
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
+			return err
+		}
+		todos = append(todos, todo)
+	}
+	return c.JSON(todos)
+}
+
+// func createTodos(c *fiber.Ctx) error {
+
+// }
+
+// func updateTodos(c *fiber.Ctx) error {
+
+// }
+
+// func deleteTodos(c *fiber.Ctx) error {
+
+// }
