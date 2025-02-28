2bb26d8e6bd9275a5563ff59a27c9f9be69a02a0
saatvik-10 <saatvikmadan529@gmail.com>
2025-02-28 19:06:17 +0530

    added delete route

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
-	// app.Delete("/api/todos/:id", deleteTodos)
+	app.Delete("/api/todos/:id", deleteTodos)
 
 	PORT := os.Getenv("PORT")
 
@@ -133,6 +133,24 @@ func updateTodos(c *fiber.Ctx) error {
 	})
 }
 
-// func deleteTodos(c *fiber.Ctx) error {
+func deleteTodos(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-// }
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Invalid ID",
+		})
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Todo deleted successfully",
+	})
+}
